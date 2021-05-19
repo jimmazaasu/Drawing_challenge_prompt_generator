908b47e40a3b781298be8f78849135c62387f907
jimmazaasu <45087818+jimmazaasu@users.noreply.github.com>
2021-05-18 22:49:47 -0400

    Added more songs to song database

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from song import Songs
 
 #The program itself
 
-song_database=Songs(["She s american","Red dress","Teenage dirtbag","Somebody Else", "El Duelo", "Rude", "F for you","Rose colored boy", "Cake pop", "Little Red Corvette"],13)
+song_database=Songs(["She s american","Red dress","Teenage dirtbag","Somebody Else", "El Duelo", "Rude", "F for you","Rose colored boy", "Cake pop", "Little Red Corvette", "Like I Love You","Love It If We Made It","Purple Rain","Girlfriend","If You re Too Shy (Let me know)","It´s Not Livin (If It´s Not With You)", "Nice For What"],13)
 
 
 
